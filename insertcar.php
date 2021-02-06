989ae7d32674b6b1bbd81eb0a7a3999f8bcccf34
Luqman <luqmanhaqeemsamsul@gmail.com>
2021-02-06 22:32:24 +0800

    update insert car interface

--- insertcar.php
+++ insertcar.php
@@ -1,146 +1,113 @@
-<?php
-	include ("header.php");
+<?php session_start();
+include("header.php");
 ?>
-<!-- forminsert.php -->
-<form method="post" action="">
-	Name:  
-	<input type="text" name="name"
-	class="form-control"><br>
-	Phone No: 
-	<input type="text" name="phoneno"
-	class="form-control"><br>
-	Gender:
-	<input type="text" name="gender"
-	class="form-control"><br>
-	Programme:
-	<input type="text" name="programme"
-	class="form-control"><br>
-	Faculty:  
-	<input type="text" name="faculty"
-	class="form-control"><br>
-	Matric No: 
-	<input type="text" name="matricno"
-	class="form-control"><br>
-	Address:
-	<input type="text" name="address"
-	class="form-control"><br>
-	Driving License Class:
-	<input type="text" name="licenseclass"
-	class="form-control"><br>
-	Road Tax End Date:  
-	<input type="text" name="roadtax"
-	class="form-control"><br>
-	Car Brand: 
-	<input type="text" name="carbrand"
-	class="form-control"><br>
-	Color:
-	<input type="text" name="color"
-	class="form-control"><br>
-	Cylinder Power:
-	<input type="text" name="cylinder power"
-	class="form-control"><br>
-	Vehicle Registration No:  
-	<input type="text" name="vehicleregistration"
-	class="form-control"><br>
-	Driver's License Expiration Date: 
-	<input type="text" name="licenseexpiration"
-	class="form-control"><br>
-	Upload License Files:
-	<input type="file" name="fileToUpload" id="fileToUpload" 
-	class="form-control"><br>
-	<input type="submit" value="Insert Record"
-	class="btn btn-success">
-</form>
-<hr>
+
+<div class="col-xl-12 col-lg-12 col-md-9 col-xs-12">
+	<div class="card shadow mb-4">
+
+		<div class="card-header py-3">
+			<div class="d-sm-flex align-items-center justify-content-between mb-1">
+				<h5 class="m-0  font-weight-bold text-primary">Insert all your information</h5>
+			</div>
+		</div>
+
+		<div class="card-body">
+
+			<div class="p-2">
+				<!-- forminsert.php -->
+				<form method="post" action="validaterequest.php" enctype="multipart/form-data">
+					<div class="form-row">
+						<div class="form-group col-md-6">
+							<label for="name">Name</label>
+							<input type="text" class="form-control" id="name" name="name" placeholder="Your name" value="<?php echo $_SESSION['fullname']; ?>" readonly>
+						</div>
+						<div class="form-group col-md-6">
+							<label for="Phone">Phone</label>
+							<input type="text" class="form-control" id="Phone" name="phoneno" placeholder="Your Phone Number" value="<?php echo $_SESSION['phone']; ?>" readonly>
+						</div>
+					</div>
+					<div class="form-row">
+						<div class="form-group col-md-6">
+							<label for="matricno">Matric Number</label>
+							<input type="text" class="form-control" id="matricno" name="matricno" placeholder="Matric number" value="<?php echo $_SESSION['matric']; ?>" readonly>
+						</div>
+						<div class="form-group col-md-6">
+							<label for="Faculty">Faculty</label>
+							<input type="text" class="form-control" id="Faculty" name="faculty" placeholder="Your Phone Number" value="<?php echo $_SESSION['fakulti']; ?>" readonly>
+						</div>
+					</div>
+					<div class="form-row">
+						<div class="form-group col-md-12">
+							<label for="programme">Program</label>
+							<input type="text" class="form-control" id="programme" name="programme" placeholder="Your name" value="<?php echo $_SESSION['program']; ?>" readonly>
+						</div>
+					</div>
+					<div class="form-row">
+						<div class="form-group col-md-12">
+							<label for="address">Address</label>
+							<input type="text" class="form-control" id="address" name="address" placeholder="Your Address" value="<?php echo $_SESSION['address']; ?>" readonly>
+						</div>
+					</div>
+					<div class="form-row">
+						<div class="form-group col-md-6">
+							<label for="licenseclass">Driving License Class</label>
+							<input type="text" class="form-control" id="licenseclass" name="licenseclass" required>
+						</div>
+						<div class="form-group col-md-6">
+							<label for="licenseexpiration">Driver's License Expiration Date</label>
+							<input type="date" class="form-control" id="licenseexpiration" name="licenseexpiration" required>
+						</div>
+					</div>
+					<div class="form-row">
+						<div class="form-group col-md-6">
+							<label for="carbrand">Car Brand</label>
+							<input type="text" class="form-control" id="carbrand" name="carbrand" required>
+						</div>
+						<div class="form-group col-md-6">
+							<label for="roadtax">Road Tax End Date</label>
+							<input type="date" class="form-control" id="roadtax" name="roadtax" required>
+						</div>
+					</div>
+					<div class="form-row">
+						<div class="form-group col-md-4">
+							<label for="color">Color</label>
+							<input type="text" class="form-control" id="color" name="color" required>
+						</div>
+						<div class="form-group col-md-4">
+							<label for="cylinderpower">Cylinder Power</label>
+							<input type="text" class="form-control" id="cylinderpower" name="cylinderpower" required>
+						</div>
+						<div class="form-group col-md-4">
+							<label for="vehicleregistration">Vehicle Registration No</label>
+							<input type="text" class="form-control" id="vehicleregistration" name="vehicleregistration" required>
+						</div>
+					</div>
+					<div class="form-row">
+						<div class="form-group col-md-4">
+							<label for="file_licence">Upload License Files</label>
+							<input type="file" class="form-control-file" id="file_licence" name="file_licence">
+						</div>
+						<div class="form-group col-md-4">
+							<label for="file_matric">Upload Matric</label>
+							<input type="file" class="form-control-file" id="file_matric" name="file_matric">
+						</div>
+						<div class="form-group col-md-4">
+							<label for="file_vehiclegrant">Upload Vehicle Grant Files</label>
+							<input type="file" class="form-control-file" id="file_vehiclegrant" name="file_vehiclegrant">
+						</div>
+						<div class="form-group col-md-4">
+							<label for="file_permisionletter">Upload Permision Letter Files</label>
+							<input type="file" class="form-control-file" id="file_permisionletter" name="file_permisionletter">
+						</div>
+					</div>
+
+					<input type="submit" value="Insert Record" name="btn_submit" class="btn btn-success">
+				</form>
+			</div>
+
+		</div>
+	</div>
+</div>
 <?php
-	if(isset($_POST['matricno']) && isset($_POST['name'])){
-
-		//connect to db
-		include ("dbconnect.php");
-
-		$name=$_POST['name'];
-		$phoneno=$_POST['phoneno'];
-		$gender=$_POST['gender'];
-		$programme=$_POST['programme'];
-		$faculty=$_POST['faculty'];
-		$matricno=$_POST['matricno'];
-		$address=$_POST['address'];
-		$drivinglicenseclass=$_POST['licenseclass'];
-		$roadtaxenddate=$_POST['roadtax'];
-		$carbrand=$_POST['carbrand'];
-		$color=$_POST['color'];
-		$cylinderpower=$_POST['cylinderpower'];
-		$vehicleregistrationno=$_POST['vehicleregistration'];
-		$driverslicenseexpirationdate=$_POST['licenseexpiration'];
-
-		//image upload only
-$target_dir = "files/";
-$target_file = $target_dir .basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-  }
-}
-
-// Check if file already exists
-if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
-  $uploadOk = 0;
-}
-
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {//bytes
-  echo "Sorry, your file is too large.";
-  $uploadOk = 0;
-}
-
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-  $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars(basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-
-  } else {
-    echo "Sorry, there was an error uploading your file.";
-  }
-}
-
-		//sql command
-		$sql="INSERT INTO cars(name,phoneno,gender,programme,faculty,matricno,
-			 address,licenseclass,roadtax,carbrand,color,cylinderpower,vehicleregistration,licenseexpiration)
-			VALUES ('$name','$phoneno','$gender','$programme','$faculty','$matricno',
-			'$address','$licenseclass','$roadtax','$carbrand','$color','$cylinderpower','$vehicleregistration',             '$licenseexpiration')";
-
-		//execute sql
-		$result = mysqli_query($conn, $sql);
-
-		//evaluate the execution
-		if($result==true){
-			echo "The record for $matricno has been saved";
-		}
-		else{
-			echo "The record cannot be saved";
-		}
-
-	}
-?>
-<?php
-	include ("footer.php");
+include("footer.php");
 ?>
